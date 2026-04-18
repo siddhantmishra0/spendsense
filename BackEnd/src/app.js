ee5583a9aaf172d2bb9c6c2777d3ec5bc094f8f7
siddhantmishra0 <mishrasiddhant2605@gmail.com>
2026-04-18 17:44:29 +0530

    env setup

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      process.env.CORS_ORIGIN || "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -26,7 +26,7 @@ app.use(
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://localhost:5173"
+    process.env.CORS_ORIGIN || "http://localhost:5173"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
