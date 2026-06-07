816d2336c08aa5478aafefaf71bb95eba406b342
siddhantmishra0 <mishrasiddhant2605@gmail.com>
2026-06-08 00:46:39 +0530

    remove hardcoded urls

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -26,7 +26,7 @@ app.use(
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    process.env.CORS_ORIGIN || "http://localhost:5173"
+    process.env.CORS_ORIGIN
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
